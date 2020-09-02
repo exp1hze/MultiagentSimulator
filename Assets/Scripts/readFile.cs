1c0f7f05fe54095687e8d9dd67d8ec93845eecbf
Ziyu <854572026@qq.com>
2020-09-01 20:55:23 -0500

    Read from params
    Put all params' value to Hashtable _params.

---
+++ Assets/Scripts/readFile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class readFile : MonoBehaviour
+{
+    public string readPath;
+    public Hashtable _params;
+    void Start()
+    {
+        _params = new Hashtable();
+        readPath = Application.dataPath + "/Tracker/params";
+        Read(readPath);
+    }
+
+    public void Read(string path)
+    {
+        StreamReader sr = new StreamReader(path, Encoding.Default);
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            BuildParams(line);
+        }
+        Debug.Log("Rerun : " + _params["Rerun"]);
+    }
+
+    public void BuildParams(string line)
+    {
+        string[] param = Regex.Split(line,"\\s+",RegexOptions.IgnoreCase);
+        _params.Add(param[0],param[1]);
+    }
+}
