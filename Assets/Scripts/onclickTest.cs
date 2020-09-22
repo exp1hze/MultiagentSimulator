c9e59902ab4a33026d9d366c483149a5159a274b
exp1hze <384877515@qq.com>
2020-09-22 00:46:29 -0500

    menu UI update

---
+++ Assets/Scripts/onclickTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onclickTest : MonoBehaviour
+{
+
+    public GameObject canvas;
+    GameObject fatherPanel ;
+
+    void Awake()
+    {
+        fatherPanel = GameObject.Find("rightPanel");
+        foreach (Transform child in fatherPanel.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+    public void onclick()
+    {
+        foreach (Transform child in fatherPanel.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        canvas.SetActive(true);
+    }
+}
