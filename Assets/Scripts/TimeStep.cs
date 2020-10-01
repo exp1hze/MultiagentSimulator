a25131d843a5d2dffbd542fd1cf3b0d6ffecb9fd
Ziyu <854572026@qq.com>
2020-09-30 22:26:29 -0500

    update

---
+++ Assets/Scripts/TimeStep.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStep : MonoBehaviour
+{
+    public int time;
+    public double target_x;
+    public double target_y;
+    public double tracker_x;
+    public double tracker_y;
+    public TimeStep(int t, double tx, double ty, double trx, double ry)
+    {
+        time = t;
+        target_x = tx;
+        target_y=ty;
+        tracker_x=trx;
+        tracker_y=ry;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/readFile.cs
+++ Assets/Scripts/readFile.cs
@@ -16,6 +16,8 @@ public class readFile : MonoBehaviour
         readPath = Application.dataPath + "/Tracker/params";
         ReadParams(readPath);
         //this.gameObject.GetComponent<writeFile>().WriteParams(readPath);
+        string rp = Application.dataPath + "/Tracker/Output/run.1/run.1.stepsummary";
+        ReadTimestep(rp);
     }
 
     public void ReadParams(string path)
@@ -34,6 +36,34 @@ public class readFile : MonoBehaviour
     {
         string[] param = Regex.Split(line,"\\s+",RegexOptions.IgnoreCase);
         _params.Add(param[0],param[1]);
+        //Debug.Log(param[0] + " " + param[1]);
+    }
+
+
+
+    public ArrayList timeSteps;
+    public void ReadTimestep(string path)
+    {
+        StreamReader sr = new StreamReader(path, Encoding.Default);
+        timeSteps = new ArrayList();
+        string line;
+        while ((line = sr.ReadLine()) != null)          //Read from top to bottom, then put variable name as key, value as value to a hashtable.
+        {
+            BuildTimestep(line);
+        }
+        foreach (TimeStep t1 in timeSteps)
+        {
+            Debug.Log(t1.time+" "+t1.target_x+" "+t1.target_y+" "+t1.tracker_x+" "+t1.tracker_y);
+        }
+        //Debug.Log("Rerun : " + _params["Rerun"]);
+        sr.Close();
+    }
+
+    public void BuildTimestep(string line)
+    {
+        string[] ts = Regex.Split(line, "\\s+", RegexOptions.IgnoreCase);
+        TimeStep t1 = new TimeStep(int.Parse(ts[2]), double.Parse(ts[4]), double.Parse(ts[5]), double.Parse(ts[7]), double.Parse(ts[8]));
+        timeSteps.Add(t1);
         //Debug.Log(param[0] + " " + param[1]);
     }
 }
