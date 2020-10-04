a5f1ec3a1e2d137a8f010ba00d767d8a44598a74
Ziyu <854572026@qq.com>
2020-10-04 18:40:32 -0500

    1004

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IDragHandler
+{
+
+    [SerializeField]
+    private Canvas canvas;
+
+    public void OnDrag(PointerEventData evenData)
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition += evenData.delta / canvas.scaleFactor;
+    }
+
+
+    void SetDraggedPosition(PointerEventData evenData)
+    {
+        var rt = gameObject.GetComponent<RectTransform>();
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, evenData.position, evenData.pressEventCamera
+            , out globalMousePos))
+        {
+            rt.position = globalMousePos;
+        }
+    }
+
+    public float scale;
+    void Start()
+    {
+        scale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (scale <= 5f)
+            {
+                scale += 0.2f;
+                setScale();
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (scale > 1f)
+            {
+                scale -= 0.2f;
+                setScale();
+            }
+        }
+    }
+
+    void setScale()
+    {
+        gameObject.GetComponent<Transform>().localScale = new Vector3(scale, scale, 1f);
+    }
+}
